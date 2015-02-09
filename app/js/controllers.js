472454f5d51d7bf4a03ce9e33460e2c8bd7101ab
malnakli <hassoun@outlook.com>
2015-02-09 17:11:38 -0500

    Two-way Data Binding

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,10 +8,15 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
     console.log($scope);
     $scope.phones = [
         {'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.'},
+            'snippet': 'Fast just got faster with Nexus S.',
+            'age': 1},
         {'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.'},
+            'snippet': 'The Next, Next Generation tablet.',
+            'age': 2},
         {'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.'}
+            'snippet': 'The Next, Next Generation tablet.',
+            'age': 3}
     ];
+    //sets the default value of orderProp to age
+    $scope.orderProp = 'age';
 });
